b7cd11d0c7a20213e7496ccc1e6a71e556d3ac14
Ken Seal <hunzinker@gmail.com>
2011-05-14 11:54:56 -0600

    Cleaned up tabs and README.

--- models/user.php
+++ models/user.php
@@ -4,67 +4,67 @@
  * User Model
  */
 class User extends AppModel {
-	var $name = 'User';
-	var $belongsTo = array('Group');
-	var $actsAs = array('Acl' => 'requester');
+    var $name = 'User';
+    var $belongsTo = array('Group');
+    var $actsAs = array('Acl' => 'requester');
 
-	var $validate = array(
-			'first_name' => array(
-				'rule' => array('minlength', 3),
-				'required' => true,
-			   	'message' => "Please enter your First Name."
+    var $validate = array(
+		'first_name' => array(
+			'rule' => array('minlength', 3),
+			'required' => true,
+			'message' => "Please enter your First Name."
 			),
-			'middle_name' => array(
-			   	'rule' => array('minlength', 1),
-                'allowEmpty' => true,
-                'message' => "Please enter your Middle Name."
+		'middle_name' => array(
+			'rule' => array('minlength', 1),
+            'allowEmpty' => true,
+            'message' => "Please enter your Middle Name."
             ),
-			'last_name' => array(
-				'rule' => array('minlength', 3),
-				'required' => true,
-			   	'message' => "Please enter your Last Name."
+		'last_name' => array(
+			'rule' => array('minlength', 3),
+			'required' => true,
+			'message' => "Please enter your Last Name."
 			),
-	       	'username' => array(
-		       	'rule' => array('alphanumeric'),
-			   	'required' => true,
-			   	'message' => 'Please make sure you use only letters and numbers.'
+	    'username' => array(
+			'rule' => array('alphanumeric'),
+			'required' => true,
+			'message' => 'Please make sure you use only letters and numbers.'
 			),
-			'email' => array(
-	           	'rule' => array('email'),
-			   	'required' => true,
-	   			'message' => 'Please make sure you have entered a valid email address.'
+		'email' => array(
+	    	'rule' => array('email'),
+			'required' => true,
+	   		'message' => 'Please make sure you have entered a valid email address.'
 	   		),
-		   	'group_id' => array(
-	           	'rule' => array('notEmpty'),
-				'required' => true,
-			   	'message' => 'Please select a User Group.'
+		'group_id' => array(
+	    	'rule' => array('notEmpty'),
+			'required' => true,
+			'message' => 'Please select a User Group.'
 			),
-           	'new_passwd' => array(
-		       	'equalTo' => array(
-			       	'rule' => array('equalTo', 'confirm_passwd'),
-				   	'message' => 'Password values do not match.'
+        'new_passwd' => array(
+			'equalTo' => array(
+				'rule' => array('equalTo', 'confirm_passwd'),
+				'message' => 'Password values do not match.'
 				),
-				'between' => array(
-					'rule' => array('between', 7, 20),
-					'allowEmpty' => true,
-					'message' => 'Your password must be between 7 and 20 characters long.'
-				)
+			'between' => array(
+				'rule' => array('between', 7, 20),
+				'allowEmpty' => true,
+				'message' => 'Your password must be between 7 and 20 characters long.'
 			)
-	);
+		)
+    );
 
 	/**
 	 * Checks to see if the username is already taken.
 	 * @return boolean
 	 */
 	function beforeValidate() {
-		if (!$this->id) {
-			$num = $this->find('count', array('conditions' => array('User.username' => $this->data['User']['username'])));
-			if ($num > 0) {
-				$this->invalidate('username_unique');
-				return false;
-			}
-		}
-		return true;
+	    if (!$this->id) {
+	        $num = $this->find('count', array('conditions' => array('User.username' => $this->data['User']['username'])));
+	        if ($num > 0) {
+	            $this->invalidate('username_unique');
+	            return false;
+	        }
+	    }
+	    return true;
 	}
 
 }
